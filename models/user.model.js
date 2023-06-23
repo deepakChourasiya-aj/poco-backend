fc3c9a74c164402790ea10ab9db8c6bcb10db7ee
deepak chourasiya <deepak1812002@gmail.com>
2023-06-23 11:12:17 +0530

    adde

--- models/user.model.js
+++ models/user.model.js
@@ -17,8 +17,7 @@ const userSchema = mongoose.Schema(
     mobile: {
       type: String,
       unique: true,
-      minlength: 10,
-      maxlength: 10,
+      minlength: 10
     },
     password: { type: String, required: true },
     role: { type: String, enum: ["patient", "doctor"], default: "patient" }
