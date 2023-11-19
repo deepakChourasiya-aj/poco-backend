db06e11757f969d992a88ddcea3e9a21798ef29e
deepak chourasiya <deepak.k@pococare.com>
2023-11-19 12:43:27 +0530

    Update : comment .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
+// Changes added successfully . 
 app.use("/api", userRoute);
 app.use("/api", accessRoute);
 app.use(authenticator);
